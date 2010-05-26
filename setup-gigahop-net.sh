570783c18332f23a4dbf79cb9f01061a513b4a0e
song10 <song10@gmail.com>
2010-05-26 10:20:14 +0800

    add setup-gigahop-net.sh

---
+++ setup-gigahop-net.sh
@@ -0,0 +1,4 @@
+sudo ifconfig eth0 up 192.168.80.197
+sudo route add default gw 192.168.80.252
+echo nameserver 168.95.1.1 | sudo tee /etc/resolv.conf > /dev/null
+echo nameserver 8.8.8.8 | sudo tee -a /etc/resolv.conf > /dev/null
