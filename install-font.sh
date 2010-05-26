1c6a395e9fccf036bfa05fc1179fc5097b325176
song10 <song10@gmail.com>
2010-05-26 08:40:51 +0800

    add install font script

---
+++ install-font.sh
@@ -0,0 +1,17 @@
+cd ~;
+if [ ! -d .fonts ]; then
+	mkdir .fonts
+fi
+
+cd .fonts
+
+curl -L -o '54390-openlogos.ttf' http://sites.google.com/site/jonnywiki/software/fonts/54390-openlogos.ttf?attredirects=0&d=1
+curl -L -o 'cwheib.ttf'          http://sites.google.com/site/jonnywiki/software/fonts/cwheib.ttf?attredirects=0&d=1
+curl -L -o 'dffn_l3.ttf'         http://sites.google.com/site/jonnywiki/software/fonts/dffn_l3.ttf?attredirects=0&d=1
+curl -L -o 'digital-7.ttf'       http://sites.google.com/site/jonnywiki/software/fonts/digital-7.ttf?attredirects=0&d=1
+curl -L -o 'LiHeiProPC.ttf'      http://sites.google.com/site/jonnywiki/software/fonts/LiHeiProPC.ttf?attredirects=0&d=1
+curl -L -o 'monaco.ttf'          http://sites.google.com/site/jonnywiki/software/fonts/monaco.ttf?attredirects=0&d=1
+curl -L -o 'msjh.ttf'            http://sites.google.com/site/jonnywiki/software/fonts/msjh.ttf?attredirects=0&d=1
+
+echo fc-cache -f -v ~/.fonts
+fc-cache -f -v ~/.fonts
