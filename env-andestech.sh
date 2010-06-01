ff7f2468a6465e70cc67b2ff919adbd04bc54ff0
song10 <song10@gmail.com>
2010-06-01 17:28:38 +0800

    add env-andestech.sh

---
+++ env-andestech.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export CVSROOT=song10@atclnx01:/home/project/PVC_CENTRALDIR
+export CVS_RSH=ssh
+export ANDESIGHT=/home/song10/opt/AndeSight
+export NDSLMGRD_LICENSE_FILE=@atcpcw03
+#export ANDESIGHT_ROOT=/home/song10/opt/AndeSight
+
+export http_proxy='http://cache1:3128'
+export https_proxy='http://cache1:3128'
