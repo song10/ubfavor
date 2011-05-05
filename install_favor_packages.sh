331bb8785c251bca756bc12d0ada51fb36f9ee80
song10 <song10@gmail.com>
2011-05-05 08:40:26 +0800

    add install_favor_packages.sh

---
+++ install_favor_packages.sh
@@ -0,0 +1,13 @@
+sudo apt-get install \
+ vim \
+ git tig \
+ meld \
+ geany \
+ openssh-server openssh-client \
+ gimp gimp-plugin-registry \
+ transmission \
+ compizconfig-settings-manager \
+ python3 \
+ default-jre \
+ stardict \
+ 
