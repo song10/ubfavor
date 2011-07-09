f345522311e452cccf7366978ee3a942fa97cfd6
song10 <song10@gmail.com>
2011-07-09 21:14:28 +0800

    add install-3rdparty-packages.sh

---
+++ install-3rdparty-packages.sh
@@ -0,0 +1,13 @@
+# install weather indicator
+sudo add-apt-repository ppa:weather-indicator-team/ppa
+#sudo apt-get update
+#sudo apt-get install indicator-weather
+
+# install cpu indicator
+sudo apt-add-repository ppa:artfwo/ppa
+#sudo apt-get update
+#sudo apt-get install indicator-cpufreq
+
+# do install
+sudo apt-get update
+sudo apt-get install indicator-weather indicator-cpufreq
