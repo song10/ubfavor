fc900a5741c7b7582a834756b464781f3da0dd4f
song10 <song10@gmail.com>
2012-06-01 01:04:48 +0800

    update install-essentail-tools.sh

--- install-essential-tools.sh
+++ install-essential-tools.sh
@@ -1,12 +1,13 @@
-sudo apt-get install \
+sudo apt-get install $* \
 	vim \
 	geany \
 	openssh-client \
 	openssh-server \
 	meld \
-	git-core \
+	git\
 	tig \
 	default-jre \
 	curl \
+    screen tmux \
 	;
 
