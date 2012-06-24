6f4b9d28492053abe52fd863bf26876738912464
song10 <song10@gmail.com>
2012-06-24 14:55:59 +0800

    install-font.sh: comment out rare use fonts

--- install-font.sh
+++ install-font.sh
@@ -5,14 +5,14 @@ fi
 
 cd .fonts
 
-curl -L -o '54390-openlogos.ttf' http://sites.google.com/site/jonnywiki/software/fonts/54390-openlogos.ttf?attredirects=0&d=1
-curl -L -o 'cwheib.ttf'          http://sites.google.com/site/jonnywiki/software/fonts/cwheib.ttf?attredirects=0&d=1
-curl -L -o 'dffn_l3.ttf'         http://sites.google.com/site/jonnywiki/software/fonts/dffn_l3.ttf?attredirects=0&d=1
-curl -L -o 'digital-7.ttf'       http://sites.google.com/site/jonnywiki/software/fonts/digital-7.ttf?attredirects=0&d=1
+#curl -L -o '54390-openlogos.ttf' http://sites.google.com/site/jonnywiki/software/fonts/54390-openlogos.ttf?attredirects=0&d=1
+#curl -L -o 'cwheib.ttf'          http://sites.google.com/site/jonnywiki/software/fonts/cwheib.ttf?attredirects=0&d=1
+#curl -L -o 'dffn_l3.ttf'         http://sites.google.com/site/jonnywiki/software/fonts/dffn_l3.ttf?attredirects=0&d=1
+#curl -L -o 'digital-7.ttf'       http://sites.google.com/site/jonnywiki/software/fonts/digital-7.ttf?attredirects=0&d=1
 curl -L -o 'LiHeiProPC.ttf'      http://sites.google.com/site/jonnywiki/software/fonts/LiHeiProPC.ttf?attredirects=0&d=1
-curl -L -o 'monaco.ttf'          http://sites.google.com/site/jonnywiki/software/fonts/monaco.ttf?attredirects=0&d=1
+#curl -L -o 'monaco.ttf'          http://sites.google.com/site/jonnywiki/software/fonts/monaco.ttf?attredirects=0&d=1
 curl -L -o 'msjh.ttf'            http://sites.google.com/site/jonnywiki/software/fonts/msjh.ttf?attredirects=0&d=1
-curl -L -o 'monaco.ttf'          http://www.gringod.com/wp-upload/MONACO.TTF
+#curl -L -o 'monaco.ttf'          http://www.gringod.com/wp-upload/MONACO.TTF
 wait
 
 echo fc-cache -f -v ~/.fonts
